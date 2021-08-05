0f70e2816d2e594cb635984094ae854356174e95
Leron Reznikov <reznikovl@gmail.com>
2021-08-05 14:48:10 -0700

    broken

--- app.py
+++ app.py
@@ -15,14 +15,21 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div([
     dcc.Dropdown(
-        id="dropdown1",
+        id="percent_dropdown",
         options=[{'label': 'True', 'value': 'True'}, {'label': 'False', 'value': 'False'}],
         value='True',
         clearable=False,
     ),
     dcc.Dropdown(
-        id="dropdown2",
+        id="quarter_dropdown",
         options=[{"label": x, "value": x} for x in reversed(get_quarters())],
+        value='Winter 2020',
+        clearable=False,
+    ),
+    dcc.Dropdown(
+        id="class_dropdown",
+        # options=[{"label": x, "value": x} for x in reversed(get_quarters())],
+        # options=[{'label': 'CMPSC 64', 'value': 'CMPSC 64'}],
         # value='Winter 2020',
         clearable=False,
     ),
@@ -30,20 +37,33 @@ app.layout = html.Div([
 ])
 
 @app.callback(
+    Output('courses', 'options'),
+    Input('quarter_dropdown', 'value')
+)
+def set_courses_options(selected_quarter):
+    return [{'label': x, 'value': x} for x in get_classes_based_off_quarter(selected_quarter)]
+@app.callback(
+    Output('class_dropdown', 'value'),
+    Input('class_dropdown', 'options')
+)
+# def set_courses_value(available_options):
+#     return available_options[0]['value']
+
+@app.callback(
     Output("bar-chart", "figure"),
-    [Input("dropdown1", "value"), Input("dropdown2", "value")]
+    [Input("percent_dropdown", "value"), Input("quarter_dropdown", "value"), Input("class_dropdown", "value")]
 )
 
 
-def update_bar_chart(day, day2):
+def update_bar_chart(percent, quarter):
     # mask = df["day"] == day
     # fig = px.bar(df[mask], x="sex", y="total_bill", 
     #              color="smoker", barmode="group")
     # return fig
     # return f"From form 1: {day1}, and from form 2: {day2}"
     # app.logger.info(plot(course='MATH 8', quarter='Fall 2020', percentage=False, professors=[]))
-    app.logger.info(day2)
-    show_percent = True if day == 'True' else False
-    return plot(course='CMPSC 64', quarter=day2, percentage=show_percent, professors=[])
+    app.logger.info(percent)
+    show_percent = True if percent == 'True' else False
+    return plot(course='CMPSC 64', quarter=quarter, percentage=show_percent, professors=[])
 
 app.run_server(debug=True)

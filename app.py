03e89a214e120b7e1d1d2d71d238f227ada788b0
Brandon Nadell <brandon.nadell@appfolio.com>
2021-09-04 13:39:08 -0700

    added professor selection options and about page

--- app.py
+++ app.py
@@ -2,69 +2,322 @@ import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 from savemygrade import *
 
 
-app = dash.Dash(__name__)
+app = dash.Dash(title='Save My Grade!', external_stylesheets=[dbc.themes.UNITED])
 
-dropdown_width='60%'
+default_store = {'stored_quarters': [], 'stored_department': 0, 'stored_number': 0, 'stored_professors': [], 'stored_percent': 0}
+default_store_index = {'stored_department': 0, 'stored_professor': 0 }
 
-dataframe = None
-default_percent = 'True'
-default_course = None
 populate_quarter_sheets()
 
 app.layout = html.Div([
-    html.Center(html.H1('Let\'s Save Your Grade!')),
-    'Quarter',
-    dcc.Checklist(
-        id="quarter_checklist",
-        options=[{"label": x, "value": x} for x in reversed(get_quarters())],
-        value=['Winter 2020'],
-        style={'width': dropdown_width},
+    dcc.Location(id='url', refresh=False),
+
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("Disbtributions", href="/")),
+            dbc.NavItem(dbc.NavLink("Professors", href="/professors")),
+            dbc.NavItem(dbc.NavLink("About", href="/about")),
+        ],
+        brand="Save My Grade!",
+        brand_href="/",
+        color="primary",
+        dark=True,
+        links_left=True,
+        fluid=True,
+        style={'padding-left': '170px'}
     ),
+    html.Div(id='page-content', style={'padding-left': '180px', 'padding-right': '180px', 'padding-top': '30px', 'padding-bottom': '0px'})
+], style={'font-family': 'Open Sans'})
+
+main_page_layout = html.Div([
+    dcc.Store(id='local', storage_type='session'),
+    html.H2('Course Distributions'),
     html.Br(),
-    'Course',
-
-    dcc.Dropdown(
-        id="class_dropdown",
-        style={'width': dropdown_width},
-        clearable=False,
-    ),
-
+    dbc.Row([
+        dbc.Col([
+            html.H6('Quarter'),
+            dcc.Dropdown(
+                id="quarter_dropdown",
+                options=[{"label": x, "value": x} for x in reversed(get_quarters())],
+                value=['Winter 2020'],
+                multi=True
+            )
+        ]),
+        dbc.Col([
+            html.H6('Dept.'),
+            dcc.Dropdown(
+                id="department_dropdown",
+                clearable=False
+            )
+        ], width=1, style={'padding-right': '0px', 'margin-right': '0px'}),
+        dbc.Col([
+            html.H6('#'),
+            dcc.Dropdown(
+                id="number_dropdown",
+                clearable=False
+            ),
+        ], width=1, style={'padding-left': '0px', 'margin-left': '0px'}),
+        dbc.Col([
+            dbc.Row([
+                html.H6('Professors'),
+                html.Div([
+                    html.Button(
+                        'Select All',
+                        id='select-all',
+                        className='btn btn-light btn-sm',
+                        style={'padding': '0.1rem 0.5rem'}
+                    )
+                ], style={'padding-left': '15px', 'margin-top': '-5px'}),
+                html.Div([
+                    html.Button(
+                        'Select None',
+                        id='select-none',
+                        className='btn btn-light btn-sm',
+                        style={'padding': '0.1rem 0.5rem'}
+                    )
+                ], style={'padding-left': '15px', 'margin-top': '-5px'})
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(
+                        id="professor_dropdown",
+                        multi=True
+                    )
+                ], style={'padding': '0px'})
+            ])
+        ], style={'padding-left': '30px', 'padding-right': '30px'}),
+        dbc.Col([
+            html.H6('Y-Axis Data'),
+            dcc.Dropdown(
+                id="percent_dropdown",
+                options=[{'label': 'Percent of Students', 'value': 'True'}, {'label': 'Number of Students', 'value': 'False'}],
+                clearable=False
+            )
+        ], width=2),
+    ]),
     html.Br(),
-    'Display Options',
-
-    dcc.Dropdown(
-        id="percent_dropdown",
-        options=[{'label': 'Show Percentage of Students', 'value': 'True'}, {'label': 'Show Number of Students', 'value': 'False'}],
-        value=default_percent,
-        style={'width': dropdown_width},
-        clearable=False,
-    ),
-
     html.Br(),
-    'Professors:',
-    dcc.Checklist(
-        id="professor_checklist",
-        labelStyle={'display': 'block'}
-        # labelStyle={'display': 'flex', 'flexFlow': 'row wrap'}
-    ),
-    dcc.Graph(id="bar-chart"),
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id="bar-chart"), width=7
+        ),
+        dbc.Col(
+            dash_table.DataTable(
+                id='statistics_table',
+                columns=[{'name': 'Professor', 'id': 'Professor'}, {'name': 'Median', 'id': 'Median'}, {'name': 'Average', 'id': 'Average'}, {'name': 'Standard Deviation', 'id': 'Standard Deviation'}],
+                sort_action="native",
+                sort_mode="multi",
+                style_table={'overflowX': 'auto'},
+                style_cell={
+                    'minWidth': '50px', 'width': '50px', 'maxWidth': '300px', 'textAlign': 'left', 'padding': '8px 8px',
+                    'font-family': 'Open Sans'
+                },
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(240, 240, 240)'
+                    }
+                ],
+                style_header={
+                    'backgroundColor': 'rgb(225, 225, 225)'
+                }
+            ), width=5
+        )
+    ])
+])
+
+# –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+@app.callback(
+    Output('quarter_dropdown', 'value'),
+    Input('quarter_dropdown', 'options'),
+    State('local', 'data')
+)
+def set_quarters_value(available_options, storage):
+    if storage and 'stored_quarters' in storage and storage['stored_quarters']:
+        return storage['stored_quarters']
+    return [available_options[0]['value']]
+
+# Department
+@app.callback(
+    Output('department_dropdown', 'options'),
+    Input('quarter_dropdown', 'value')
+)
+def set_departments_options(selected_quarters):
+    if selected_quarters == []:
+        raise PreventUpdate
+    return [{'label': x, 'value': x} for x in get_departments_based_off_quarter(selected_quarters)]
+
+@app.callback(
+    Output('department_dropdown', 'value'),
+    Input('department_dropdown', 'options'),
+    State('local', 'data')
+)
+def set_departments_value(available_options, storage):
+    if storage and 'stored_department' in storage and storage['stored_department'] in [h['value'] for h in available_options]:
+        return storage['stored_department']
+    return available_options[0]['value']
+
+# Number
+@app.callback(
+    Output('number_dropdown', 'options'),
+    Input('quarter_dropdown', 'value'),
+    Input('department_dropdown', 'value')
+)
+def set_numbers_options(selected_quarters, selected_department):
+    if selected_quarters == []:
+        raise PreventUpdate
+    numbers = get_numbers_based_off_quarters_and_department(selected_quarters, selected_department)
+    return [{'label': x, 'value': x} for x in numbers]
+
+@app.callback(
+    Output('number_dropdown', 'value'),
+    Input('number_dropdown', 'options'),
+    State('local', 'data')
+)
+def set_numbers_value(available_options, storage):
+    if available_options == []:
+        raise PreventUpdate
+    if storage and 'stored_number' in storage and storage['stored_number'] in [h['value'] for h in available_options]:
+        return storage['stored_number']
+    return available_options[0]['value']
+
+# Professor
+@app.callback(
+    Output('professor_dropdown', 'options'),
+    Input('quarter_dropdown', 'value'),
+    Input('department_dropdown', 'value'),
+    Input('number_dropdown', 'value')
+)
+def set_professor_options(selected_quarters, selected_department, selected_number):
+    if selected_quarters == []:
+        raise PreventUpdate
+    professors = get_professor_based_off_class_and_quarter(selected_department + " " + selected_number, selected_quarters)
+    return [{'label': x, 'value': x} for x in professors]
+
+@app.callback(
+    Output('professor_dropdown', 'value'),
+    Output('select-all', 'n_clicks'),
+    Output('select-none', 'n_clicks'),
+    Input('professor_dropdown', 'options'),
+    Input('select-all', 'n_clicks'),
+    Input('select-none', 'n_clicks'),
+    State('local', 'data'),
+)
+def set_professor_value(available_options, all_clicks, none_clicks, storage):
+    if all_clicks:
+        return [i['value'] for i in available_options], 0, 0
+    elif none_clicks:
+        return [], 0, 0
+    else:
+        if available_options == []:
+            return []
+        professors = []
+        for o in available_options:
+            if storage and 'stored_professors' in storage and o['value'] in storage['stored_professors']:
+                professors.append(o['value'])
+        if professors == []:
+            return [available_options[0]['value']], 0, 0
+        return professors, 0, 0
+
+# Percent
+@app.callback(
+    Output('percent_dropdown', 'value'),
+    Input('percent_dropdown', 'options'),
+    State('local', 'data')
+)
+def set_percent_value(available_options, storage):
+    if storage and 'stored_percent' in storage and storage['stored_percent']:
+        return storage['stored_percent']
+    return available_options[0]['value']
+
+# Plot
+@app.callback(
+    Output("bar-chart", "figure"),
+    Output("bar-chart", "config"),
+    Output('statistics_table', 'data'),
+    Output('local', 'data'),
+    Input("percent_dropdown", "value"),
+    Input("quarter_dropdown", "value"),
+    Input("department_dropdown", "value"),
+    Input("number_dropdown", "value"),
+    Input('professor_dropdown', 'value'),
+    State('local', 'data')
+)
+def update_bar_chart(percent, quarters, department, number, professors, storage):
+    if quarters == []:
+        raise PreventUpdate
+    if not storage or storage.keys is not default_store.keys:
+        storage = default_store
+    storage['stored_quarters'] = quarters
+    storage['stored_department'] = department
+    storage['stored_number'] = number
+    storage['stored_percent'] = percent
+    storage['stored_professors'] = professors
+    fig, statistics = plot(course=department+" "+number, quarters=quarters, percentage=percent=='True', professors=professors)
+    config = {'displayModeBar': False}
+    return fig, config, statistics, storage
+
+# –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+professor_page_layout = html.Div([
+    dcc.Store(id='local_index', storage_type='session'),
+    html.H2('Professor History'),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H6('Department'),
+            dcc.Dropdown(
+                id="department_dropdown_index",
+                options=[{"label": x, "value": x} for x in get_departments_based_off_quarter(get_quarters())]
+            )
+        ], width=2),
+        dbc.Col([
+            html.H6('Professor'),
+            dcc.Dropdown(
+                id="professor_dropdown_index"
+            )
+        ], width=2)
+    ]),
+    html.Br(),
     dash_table.DataTable(
-        id='statistics_table',
-        columns=[{'name': 'Professor', 'id': 'Professor'}, {'name': 'Median', 'id': 'Median'}, {'name': 'Average', 'id': 'Average'}, {'name': 'Standard Deviation', 'id': 'Standard Deviation'}],
-        style_table={'overflowX': 'auto', 'width': '750px'},
+        id='statistics_table_index',
+        columns=[{'name': 'Quarter', 'id': 'Quarter'}, {'name': 'Course', 'id': 'Course'}, {'name': 'Median', 'id': 'Median'}, {'name': 'Average', 'id': 'Average'}, {'name': 'Standard Deviation', 'id': 'Standard Deviation'}],
+        sort_action="native",
+        sort_mode="multi",
+        style_table={'overflowX': 'auto', 'maxWidth': '600px'},
         style_cell={
-            'minWidth': '150px', 'width': '150px', 'maxWidth': '300px', 'textAlign': 'left',
+            'minWidth': '50px', 'maxWidth': '300px', 'textAlign': 'left', 'padding': '8px 8px',
+            'font-family': 'Open Sans'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(240, 240, 240)'
+            }
+        ],
+        style_header={
+            'backgroundColor': 'rgb(225, 225, 225)'
+        }
+    ),
+    html.Br(),
+    dash_table.DataTable(
+        id='statistics_table_index_cumulative',
+        columns=[{'name': 'Overall Median', 'id': 'Overall Median'}, {'name': 'Overall Average', 'id': 'Overall Average'}, {'name': 'Overall Standard Deviation', 'id': 'Overall Standard Deviation'}],
+        sort_action="native",
+        sort_mode="multi",
+        style_table={'overflowX': 'auto', 'maxWidth': '600px'},
+        style_cell={
+            'minWidth': '150px', 'maxWidth': '300px', 'textAlign': 'left', 'padding': '8px 8px',
             'font_family': 'Open Sans'
         },
-        style_cell_conditional=[
-            {'if': {'column_id': 'Professor'},
-            'width': '200%'}
-        ],
         style_data_conditional=[
             {
                 'if': {'row_index': 'odd'},
@@ -77,61 +330,75 @@ app.layout = html.Div([
     )
 ])
 
+about_page_layout = html.Div([
+    dcc.Store(id='local_index', storage_type='session'),
+    html.H2('About'),
+    html.Br(),
+    html.H6('Save My Grade is a web application to view grade distributions and professor statistics for UCSB. Data is currently available from Fall 2015 to Winter 2021'),
+    html.Br(),
+    html.H6('Developed by Brandon Nadell and Leron Reznikov')
+])
+
+# Quarter
 @app.callback(
-    Output('class_dropdown', 'options'),
-    Input('quarter_checklist', 'value')
+    Output('department_dropdown_index', 'value'),
+    Input('department_dropdown_index', 'options'),
+    State('local_index', 'data')
 )
-
-def set_courses_options(selected_quarters):
-    return [{'label': x, 'value': x} for x in get_classes_based_off_quarter(selected_quarters)]
-
-@app.callback(
-    Output('class_dropdown', 'value'),
-    Input('class_dropdown', 'options')
-)
-
-def set_courses_value(available_options):
-    if default_course in [h['value'] for h in available_options]:
-        return default_course
+def set_department_index_value(available_options, storage):
+    if storage and 'stored_department' in storage:
+        return storage['stored_department']
     return available_options[0]['value']
 
+# Professor
 @app.callback(
-    Output('professor_checklist', 'options'),
-    Input('quarter_checklist', 'value'),
-    Input('class_dropdown', 'value')
+    Output('professor_dropdown_index', 'options'),
+    Input('department_dropdown_index', 'value')
 )
-
-def set_professor_options(selected_quarters, selected_class):
-    global dataframe
-    professors, dataframe = get_professor_based_off_class_and_quarter(selected_class, selected_quarters)
-    return [{'label': x, 'value': x} for x in professors]
+def set_professor_index_options(selected_department):
+    return [{'label': x, 'value': x} for x in get_professor_based_off_department(selected_department)]
 
 @app.callback(
-    Output('professor_checklist', 'value'),
-    Input('professor_checklist', 'options')
+    Output('professor_dropdown_index', 'value'),
+    Input('professor_dropdown_index', 'options'),
+    State('local_index', 'data')
 )
+def set_professor_index_value(available_options, storage):
+    if storage and 'stored_professor' in storage and storage['stored_professor'] in [h['value'] for h in available_options]:
+        return storage['stored_professor']
+    return available_options[0]['value']
 
-def set_professor_value(available_options):
-    return [available_options[0]['value']]
-
+# Table
 @app.callback(
-    Output("bar-chart", "figure"),
-    [Input("percent_dropdown", "value"), Input("quarter_checklist", "value"), Input("class_dropdown", "value"), Input('professor_checklist', 'value')]
+    Output('statistics_table_index', 'data'),
+    Output('statistics_table_index_cumulative', 'data'),
+    Output('local_index', 'data'),
+    Input('department_dropdown_index', 'value'),
+    Input('professor_dropdown_index', 'value'),
+    State('local_index', 'data')
 )
 
-def update_bar_chart(percent, quarters, course, professors):
-    global default_percent, default_course
-    default_percent = percent
-    default_course = course
-    show_percent = True if percent == 'True' else False
-    return plot(course=course, quarters=quarters, percentage=show_percent, professors=professors, dataframe=dataframe)
+def set_table(department, professor, storage):
+    if not storage or storage.keys is not default_store_index.keys:
+        storage = default_store_index
+    storage['stored_department'] = department
+    storage['stored_professor'] = professor
+    stats = get_statistics_of_professor(professor)
+    return stats[0], stats[1], storage
 
-@app.callback(
-    Output('statistics_table', 'data'),
-    Input('bar-chart', 'figure')
-)
 
-def update_table(figure):
-    return getStatistics()
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return main_page_layout
+    elif pathname == '/professors':
+        return professor_page_layout
+    elif pathname == '/about':
+        return about_page_layout
+    # You could also return a 404 "URL not found" page here
 
-app.run_server(debug=True, host='0.0.0.0')
+app.run_server(debug=True
+,dev_tools_ui=False
+, host='0.0.0.0')
+# app.config.suppress_callback_exceptions = True
--- savemygrade.py
+++ savemygrade.py
@@ -1,27 +1,48 @@
-from os import stat
 import numpy as np
 import pandas as pd
 import plotly.express as px
 from collections import Counter
 
+labels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F']
+gpas = [4.0, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0]
+
 file = pd.ExcelFile(r'grades.xlsx')
 quarter_sheets = dict()
+quarter_sheets_no_quarter = dict()
 statistics = []
 
 def get_quarters():
-    return file.sheet_names
+    return ['Winter 2021']
+    # return file.sheet_names
 
 def populate_quarter_sheets():
     if len(quarter_sheets) == 0:
         for q in get_quarters():
             d = file.parse(q)
-            d['Instructor'] = d['Instructor'] + ' (' + q + ')'
             d.Course = d.Course.replace('\s+', ' ', regex=True)
+            d.Course = d.Course.str.replace('ES 1-', 'ES', regex=False)
+            d.Course = d.Course.str.replace('(?<=ED .) ', '', regex=True)
+            d.Course = d.Course.str.replace('(?<=ED .{2}) ', '', regex=True)
+            quarter_sheets_no_quarter[q] = d.copy()
+            d['Instructor'] = d['Instructor'] + ' (' + q + ')'
             quarter_sheets[q] = d
 
 def get_classes_based_off_quarter(quarters):
     data = pd.concat([quarter_sheets[q] for q in quarters])
     return data['Course'].unique()
+
+def get_departments_based_off_quarter(quarters):
+    data = pd.concat([quarter_sheets[q] for q in quarters])
+    departments = [d[:d.rfind(' ')] for d in data['Course'].unique()]
+    return sorted(list(set(departments)))
+
+def get_numbers_based_off_quarters_and_department(quarters, department):
+    common_course_numbers = None
+    for q in quarters:
+        courses = [c for c in quarter_sheets[q]['Course'].unique() if c[:c.rfind(' ')] == department]
+        course_numbers = [c.split(' ')[-1] for c in courses]
+        common_course_numbers = course_numbers if not common_course_numbers else intersection(common_course_numbers, course_numbers)
+    return common_course_numbers
 
 def get_professor_based_off_class_and_quarter(course, quarters):
     data = pd.concat([quarter_sheets[q] for q in quarters])
@@ -35,13 +56,13 @@ def get_professor_based_off_class_and_quarter(course, quarters):
             unique_professors.append(p)
         else:
             unique_professors.append(p.split(' (')[0])
-            only_course = only_course.replace(p, p.split(' (')[0])
     professors = unique_professors
 
-    return [professors, only_course]
+    return professors
 
 def avg(counts, gpas):
-    return np.sum(np.dot(counts, gpas)) / np.sum(counts)
+    total = np.sum(counts)
+    return np.sum(np.dot(counts, gpas)) / total
 
 def median(counts, gpas):
     n = 0
@@ -58,27 +79,70 @@ def std_dev(counts, gpas):
         result += ((gpas[i] - mean)**2) * counts[i]
     return (result/np.sum(counts))**(0.5)
 
+def intersection(lst1, lst2):
+    temp = set(lst2)
+    return [value for value in lst1 if value in temp]
 
-def getStatistics():
-    return statistics
+def get_professor_based_off_department(department):
+    data = pd.concat([quarter_sheets_no_quarter[q] for q in get_quarters()])[['Course', 'Instructor']]
+    data = data[data['Course'].str.startswith(department)]
+
+    professors = data['Instructor'].unique().tolist()
+    professors = [str(s) for s in professors]
+    return sorted(professors)
+
+def get_statistics_of_professor(professor):
+    statistics = []
+    total_counts = pd.DataFrame()
+    for q in get_quarters():
+        data = quarter_sheets_no_quarter[q]
+        data = data[data['Instructor'] == professor]
+
+        for course in data['Course'].unique().tolist():
+            df = data[data['Course'] == course][['Grade Given', 'Sum of Student Count']]
+
+            df.columns = ['Grade', professor]
+            counts = pd.DataFrame({'Grade': labels}).set_index('Grade').join(df.set_index('Grade'))[professor].fillna(0)
+            if total_counts.empty:
+                total_counts = counts
+            else:
+                total_counts = total_counts + counts
+
+            med = median(counts, gpas) or 'N/A'
+            mean = mean if not np.isnan(mean := round(avg(counts, gpas), 2)) else 'N/A'
+            dev = dev if not np.isnan(dev := round(std_dev(counts, gpas), 2)) else 'N/A'
+            statistics.append({'Quarter': q, 'Course': course, 'Median': str(med), 'Average': str(mean), 'Standard Deviation': str(dev)})
+
+    med = median(total_counts, gpas) or 'N/A'
+    mean = mean if not np.isnan(mean := round(avg(total_counts, gpas), 2)) else 'N/A'
+    dev = dev if not np.isnan(dev := round(std_dev(total_counts, gpas), 2)) else 'N/A'
+    return statistics, [{'Overall Median': str(med), 'Overall Average': str(mean), 'Overall Standard Deviation': str(dev)}]
 
 #——————————————————————————————————————————————————————————#
 
-def plot(course, quarters, professors, percentage, dataframe):
-    labels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F']
-    gpas = [4.0, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0]
+def plot(course, quarters, professors, percentage):
+    data = pd.concat([quarter_sheets[q] for q in quarters])
+    only_course = data[data['Course'] == course]
+    
+    new_professors = only_course['Instructor'].tolist()
+    for i in range(len(new_professors)):
+        if new_professors[i].split(' (')[0] in professors:
+            new_professors[i] = new_professors[i].split(' (')[0]
+    only_course = only_course.assign(Instructor=new_professors)
+
     cols = pd.DataFrame({'Grade': labels})
 
-    # if not professors or len(professors) == 0:
-    #     do something
     statistics.clear()
 
     for professor in professors:
-        other = dataframe[dataframe['Instructor'] == professor][['Grade Given', 'Sum of Student Count']]
+        other = only_course[only_course['Instructor'] == professor][['Grade Given', 'Sum of Student Count']]
         other.columns = ['Grade', professor]
 
         counts = pd.DataFrame({'Grade': labels}).set_index('Grade').join(other.set_index('Grade'))[professor].fillna(0)
-        statistics.append({'Professor': professor, 'Median': str(median(counts, gpas)), 'Average': str(round(avg(counts, gpas), 2)), 'Standard Deviation': str(round(std_dev(counts, gpas), 2))})
+        med = median(counts, gpas) or 'N/A'
+        mean = mean if not np.isnan(mean := round(avg(counts, gpas), 2)) else 'N/A'
+        dev = dev if not np.isnan(dev := round(std_dev(counts, gpas), 2)) else 'N/A'
+        statistics.append({'Professor': professor, 'Median': str(med), 'Average': str(mean), 'Standard Deviation': str(dev)})
 
         if percentage:
             other[professor] = other[professor].div(np.sum(other[professor]), axis=0)
@@ -95,9 +159,34 @@ def plot(course, quarters, professors, percentage, dataframe):
             'value': 'Percentage of Students' if percentage else 'Number of Students',
             'variable': 'Professor'
         },
-        title=course,
-        barmode='group'
+        barmode='group',
+        color_discrete_sequence=px.colors.qualitative.Set2
     )
+    fig.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="right",
+        x=0.99,
+        
+    ), margin=dict(l=20, r=0, t=0, b=0))
+    fig.update_xaxes(title=None, fixedrange=True)
+    fig.update_yaxes(fixedrange=True)
     if percentage:
         fig.update_layout(yaxis_tickformat = '%')
-    return fig
+    if np.all((fig['data'][0]['y'] == 0)):
+        fig.update_layout(
+            xaxis =  { "visible": False },
+            yaxis = { "visible": False },
+            annotations = [
+                {   
+                    "text": "No data found",
+                    "xref": "paper",
+                    "yref": "paper",
+                    "showarrow": False,
+                    "font": {
+                        "size": 28
+                    }
+                }
+            ]
+        )
+    return fig, statistics
